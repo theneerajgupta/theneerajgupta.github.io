a7d4a1071d8d160545b9483bebc421bca20f3782
Neeraj Gupta <theneerajgupta@gmail.com>
2025-08-08 10:52:42 +0530

    Added CSS Reset and Normalize CSS

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import 'normalize.css';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.scss';
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
