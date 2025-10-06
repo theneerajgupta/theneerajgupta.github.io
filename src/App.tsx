bc88c4ab49c7663d29317304555f29a3ae0af49b
Neeraj Gupta <theneerajgupta@gmail.com>
2025-10-07 00:06:45 +0530

    Initiated New Portfolio Website

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className='bg-base-300'>
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
